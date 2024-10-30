4cc417cf3be9b4a49f5350e39d467d170249ade7
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-10-30 21:08:03 +0100

    Summary of useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import './App.css'
 import HookCounter from './components/HookCounter'
 import HookCounter2 from './components/HookCounter2'
 import HookCounter3 from './components/HookCounter3'
+import HookCounter4 from './components/HookCounter4'
 
 function App() {
 
   return (
     <>
-    {/* <HookCounter /> */}
-    {/* <HookCounter2 /> */}
-    <HookCounter3 />
+    {/* <HookCounter />
+    <HookCounter2 />
+    <HookCounter3 /> */}
+    <HookCounter4 />
     </>
   )
 }
--- src/components/HookCounter3.jsx
+++ src/components/HookCounter3.jsx
@@ -8,12 +8,12 @@ const HookCounter3 = () => {
         <input 
         type="text" 
         value={name.firstname} 
-        onChange={e => setName({firstname: e.target.value})}
+        onChange={e => setName({ ...name, firstname: e.target.value})}
         />
         <input 
         type="text" 
         value={name.lastname} 
-        onChange={e => setName({lastname: e.target.value})}
+        onChange={e => setName({ ...name,lastname: e.target.value})}
         />
 
         <h2>Your Firstname is -{name.firstname}</h2>
