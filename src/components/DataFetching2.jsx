6352053a55b4cb0b55a8149f2127367823688b17
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-05 00:30:19 +0100

    conclusion on Data fetching using useState

--- src/components/DataFetching2.jsx
+++ src/components/DataFetching2.jsx
@@ -4,6 +4,11 @@ import axios from 'axios'
 function DataFetching2() {
     const [post, setPost] = useState({})
     const [id, setId] = useState(1)
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
 
     useEffect (() => {
         axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -14,10 +19,11 @@ function DataFetching2() {
         .catch(err => {
             console.log(err)
         })
-    },[id])
+    },[idFromButtonClick])
   return (
     <div>
         <input type='text'  value={id} onChange={e => setId(e.target.value)}/>
+        <button type='button' onClick={handleClick}>Fetch Post</button>
         <div>{post.title}</div>
         {/* <ul>
             {
